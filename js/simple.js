56b9b00d97728565709b0472de1be0bff1b905fc
Justin Golden <justingolden21@gmail.com>
2020-04-11 01:47:54 -0700

    moved index.html to player.hmtl under legacy tab
    took some fixes. simple scripts will eventually be scripts for player-ui and player.css should be merged with styles or renamed to player-ui.css. simple.js had redundatn code commetned as well as code that edits html elements with the same names. will fix that in next update

--- js/simple.js
+++ js/simple.js
@@ -12,47 +12,48 @@ let inventory = {
 // SGI_46
 // SGI_177
 
-const icons = {
-	'money' : 'SGI_59',
-	'bread' : 'SGI_164',
-	'ore'   : 'SGI_65',
-	'lumber': 'SGI_122',
-	'metal' : 'SGI_84',
-	'wheat' : 'SGI_62',
-	'flour' : 'SGI_158',
-	'tools' : 'SGI_24',
+// in sprites.js
+// const icons = {
+// 	'money' : 'SGI_59',
+// 	'bread' : 'SGI_164',
+// 	'ore'   : 'SGI_65',
+// 	'lumber': 'SGI_122',
+// 	'metal' : 'SGI_84',
+// 	'wheat' : 'SGI_62',
+// 	'flour' : 'SGI_158',
+// 	'tools' : 'SGI_24',
 
-	'mine'	  : 'SGI_89',
-	'smith'	  : 'SGI_70',
-	'forester': 'SGI_110',
-	'farm'	  : 'SGI_159',
-	'mill'	  : 'SGI_49',
-	'baker'	  : 'SGI_75',
-	'refinery': 'SGI_121',
-	'mint'	  : 'SGI_144'
-};
+// 	'mine'	  : 'SGI_89',
+// 	'smith'	  : 'SGI_70',
+// 	'forester': 'SGI_110',
+// 	'farm'	  : 'SGI_159',
+// 	'mill'	  : 'SGI_49',
+// 	'baker'	  : 'SGI_75',
+// 	'refinery': 'SGI_121',
+// 	'mint'	  : 'SGI_144'
+// };
 
 const firms = 'mine smith forester farm mill baker refinery mint'.split(' ');
 
-// also in display.js
-const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+// in display.js
+// const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-window.onload = ()=> {
+$( ()=> {
 
 	let tmpHTML = '';
 	for(item in inventory)
 		tmpHTML += capitalize(item) + ': ' + 0 + ' <img src="img/icons/' + icons[item] + '.png" class="icon-sm"> | ';
-	$('#prices').html(tmpHTML);
+	// $('#prices').html(tmpHTML);
 
 	tmpHTML = '';
 	for(item in inventory)
 		tmpHTML += capitalize(item) + ': ' + inventory[item] + ' <img src="img/icons/' + icons[item] + '.png" class="icon-sm"> | ';
-	$('#total-resources').html(tmpHTML);
+	// $('#total-resources').html(tmpHTML);
 
 	tmpHTML = '';
 	for(idx in firms)
 		tmpHTML += capitalize(firms[idx]) + ': ' + 0 + ' <img src="img/icons/' + icons[firms[idx] ] + '.png" class="icon-sm"> | ';
-	$('#firm-types').html(tmpHTML);
+	// $('#firm-types').html(tmpHTML);
 
 	fillPlayerInputTable();
 
@@ -71,7 +72,7 @@ window.onload = ()=> {
 	$('#player-inventory').append('TODO: pie chart for types of resources');
 
 
-}
+});
 
 function fillPlayerInputTable() {
 	let tmpHTML = '<tr><td>Resource</td><td></td><td>Buy/Sell</td><td>Amount</td><td>Price</td></tr>';
