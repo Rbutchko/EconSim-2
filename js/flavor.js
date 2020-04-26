05a2e53e582ac778347f9705865e8f10ca0e4932
RGB Studios <justingolden21@gmail.com>
2020-04-26 00:41:43 -0700

    add flavor.js

---
+++ js/flavor.js
@@ -0,0 +1,18 @@
+const FLAVOR = {
+	bread: 'Lipsum',
+	ore: 'Lipsum',
+	lumber: 'Lipsum',
+	metal: 'Lipsum',
+	wheat: 'Lipsum',
+	flour: 'Lipsum',
+	tools: 'Lipsum',
+
+	mine: 'Lipsum',
+	smith: 'Lipsum',
+	forester: 'Lipsum',
+	farm: 'Lipsum',
+	mill: 'Lipsum',
+	baker: 'Lipsum',
+	refinery: 'Lipsum',
+	mint: 'Lipsum',
+}
